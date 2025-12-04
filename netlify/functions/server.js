a1fa8d5c98c7a9b6307da3585789813775331427
Shudufhadzo Tshimuka <shudufhadzo118@gmail.com>
2025-12-04 03:02:08 +0200

    Add files via upload

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -22,7 +22,7 @@ export const handler = async (event) => {
 
     // Use Node 18+ built-in fetch (no import)
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
